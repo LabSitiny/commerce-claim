c63e4c65fec16beeaa1bb579b006996ebeb54a16
Parkseongheum <ab6449@gmail.com>
2022-11-27 23:55:50 +0900

    Init: Project claim service

---
+++ src/main/kotlin/dev/labsitiny/commerceclaim/CommerceClaimApplication.kt
@@ -0,0 +1,11 @@
+package dev.labsitiny.commerceclaim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceClaimApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommerceClaimApplication>(*args)
+}
---
+++ src/test/kotlin/dev/labsitiny/commerceclaim/CommerceClaimApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.labsitiny.commerceclaim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceClaimApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
